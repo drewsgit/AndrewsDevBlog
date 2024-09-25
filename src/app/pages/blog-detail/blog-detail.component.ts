d2a58e5fc606fb464420b43be947e791bbd4ae27
Andrew Wong <andrewlwong2@yahoo.com>
2024-09-25 14:19:17 -0400

    Integrated blog details with api

--- src/app/pages/blog-detail/blog-detail.component.ts
+++ src/app/pages/blog-detail/blog-detail.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 
 import { FooterComponent } from '../../components/footer/footer/footer.component';
 import { ScrollToTopComponent } from '../../components/scroll-to-top/scroll-to-top.component';
 import { NavbarComponent } from '../../components/navbar/navbar/navbar.component';
 
-import BlogData from '../../data/business.json'
+import { DbService, IPost } from '../../data/db.service';
 
 @Component({
   selector: 'app-blog-detail',
@@ -22,7 +22,7 @@ import BlogData from '../../data/business.json'
   styleUrl: './blog-detail.component.scss'
 })
 export class BlogDetailComponent {
-  blogData = BlogData
+  dbService = inject(DbService);
 
   constructor(private router: ActivatedRoute) { }
 
@@ -30,6 +30,12 @@ export class BlogDetailComponent {
   data: any
   ngOnInit(): void {
     this.id = this.router.snapshot.params['id']
-    this.data = this.blogData.find((item: any) => item.id === parseInt(this.id))
+
+    this.dbService.getPostByID(this.id).subscribe((res: IPost[]) => {
+      console.log(res);
+      if (res.length > 0) {
+        this.data = res[0];
+      }
+    })
   }
 }

862af0097ed5c74cd049d0525e9d01bc2ea0c5ed
Andrew Wong <andrewlwong2@yahoo.com>
2024-10-09 23:38:48 -0400

    Using Angular Material spinner

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -3,11 +3,17 @@ import { Component, OnInit } from "@angular/core";
 import { RouterLink, RouterOutlet } from "@angular/router";
 import { DbService, IPost } from "../../data/db.service";
 import { ProgressSpinnerModule } from "primeng/progressspinner";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
 @Component({
   selector: "app-index",
   standalone: true,
-  imports: [CommonModule, RouterLink, ProgressSpinnerModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    ProgressSpinnerModule,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: "./index.component.html",
   styleUrl: "./index.component.scss",
 })

90fe6bc7d5a70f14b3aeb78fb87f70f94d7236d8
Andrew Wong <andrewlwong2@yahoo.com>
2024-10-24 09:03:42 -0400

    Add form in progress

--- src/app/pages/admin/post/add-edit-post/add-edit-post.component.ts
+++ src/app/pages/admin/post/add-edit-post/add-edit-post.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component, inject, OnInit } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { ActivatedRoute, RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-add-edit-post',
+  selector: "app-add-edit-post",
   standalone: true,
-  imports: [],
-  templateUrl: './add-edit-post.component.html',
-  styleUrl: './add-edit-post.component.scss'
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
+  templateUrl: "./add-edit-post.component.html",
+  styleUrl: "./add-edit-post.component.scss",
 })
-export class AddEditPostComponent {
+export class AddEditPostComponent implements OnInit {
+  route = inject(ActivatedRoute);
+  fb = inject(FormBuilder);
 
+  forms = this.fb.group({
+    title: ["", Validators.required],
+    subtitle: [""],
+    body: [""],
+  });
+
+  ngOnInit(): void {
+    const queryParams = this.route.snapshot.queryParams;
+    console.log(queryParams["type"]);
+  }
+
+  submit() {
+    alert("submit");
+  }
+
+  reset() {
+    this.forms.reset();
+  }
 }

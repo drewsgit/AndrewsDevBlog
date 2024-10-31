e9e4858b4d58c1dad6849915ce51eae96da1aa4a
drewsgit <andrewlwong2@yahoo.com>
2024-10-31 09:22:45 -0400

    - Added paginator for admin posts, - clean up resources

--- src/app/pages/admin/post/post-table/post-table.component.ts
+++ src/app/pages/admin/post/post-table/post-table.component.ts
@@ -6,18 +6,32 @@ import {
   ViewEncapsulation,
 } from "@angular/core";
 import { DbService, IPost } from "../../../../data/db.service";
-import { finalize, Observable, Subject, takeUntil } from "rxjs";
+import {
+  catchError,
+  finalize,
+  forkJoin,
+  Observable,
+  Subject,
+  takeUntil,
+} from "rxjs";
 import { TableModule } from "primeng/table";
 import { CommonModule } from "@angular/common";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { Router, RouterLink } from "@angular/router";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import { DeletePostConfirmComponent } from "./delete-post-confirm/delete-post-confirm.component";
+import { PaginatorModule, PaginatorState } from "primeng/paginator";
 
 @Component({
   selector: "app-post",
   standalone: true,
-  imports: [TableModule, CommonModule, MatProgressSpinnerModule, RouterLink],
+  imports: [
+    TableModule,
+    CommonModule,
+    MatProgressSpinnerModule,
+    RouterLink,
+    PaginatorModule,
+  ],
   templateUrl: "./post-table.component.html",
   styleUrl: "./post-table.component.scss",
   encapsulation: ViewEncapsulation.None,
@@ -30,6 +44,10 @@ export class PostTableComponent implements OnInit, OnDestroy {
   destroy$: Subject<boolean> = new Subject<boolean>();
   posts$: IPost[] = [];
   inProgress = false;
+
+  first: number = 0; // Count of first record in page
+  rowsPerPage: number = 10; // Number of records per page
+  totalRecords: number = 0; // Total number of records
 
   ngOnInit(): void {
     this.loadTable();
@@ -69,19 +87,25 @@ export class PostTableComponent implements OnInit, OnDestroy {
 
   private loadTable() {
     this.inProgress = true;
-    this.dbService
-      .getPosts()
+
+    forkJoin({
+      posts$: this.dbService.getPostsWithPaging(this.rowsPerPage, this.first),
+      count$: this.dbService.getPostsCount(),
+    })
       .pipe(
         takeUntil(this.destroy$),
-        finalize(() => (this.inProgress = false))
+        catchError((error) => {
+          console.log(error);
+          this.inProgress = false;
+          throw error;
+        })
       )
-      .subscribe({
-        error: (err) => {
-          console.log("Error: ", err);
-        },
-        next: (res) => {
-          this.posts$ = res;
-        },
+      .subscribe((res) => {
+        // console.log('post data:', res.posts$);
+        // console.log('count data:', res.count$);
+        this.posts$ = res.posts$;
+        this.totalRecords = res.count$;
+        this.inProgress = false;
       });
   }
 
@@ -89,6 +113,12 @@ export class PostTableComponent implements OnInit, OnDestroy {
     this.router.navigate(["/admin/add-edit-post"], {
       queryParams: { type: "edit", id: post.id },
     });
+  }
+
+  onPageChange(event: PaginatorState) {
+    this.first = event.first;
+    this.rowsPerPage = event.rows;
+    this.loadTable();
   }
 
   ngOnDestroy(): void {
--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,18 +1,11 @@
 import { CommonModule } from "@angular/common";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { RouterLink, RouterOutlet } from "@angular/router";
 import { DbService, IPost } from "../../data/db.service";
 import { ProgressSpinnerModule } from "primeng/progressspinner";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { PaginatorModule, PaginatorState } from "primeng/paginator";
-import { forkJoin } from "rxjs";
-
-interface PageEvent {
-  first: number;
-  rows: number;
-  page: number;
-  pageCount: number;
-}
+import { catchError, forkJoin, Subject, takeUntil } from "rxjs";
 
 @Component({
   selector: "app-index",
@@ -27,39 +20,51 @@ interface PageEvent {
   templateUrl: "./index.component.html",
   styleUrl: "./index.component.scss",
 })
-export class IndexComponent implements OnInit {
+export class IndexComponent implements OnInit, OnDestroy {
   posts: IPost[] = [];
   recentPosts: IPost[] = [];
-  first: number = 0;  // Count of first record in page
-  rowsPerPage: number = 5;  // Number of records per page
+  first: number = 0; // Count of first record in page
+  rowsPerPage: number = 5; // Number of records per page
   totalRecords: number = 0; // Total number of records
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(private dbService: DbService) {}
 
   ngOnInit(): void {
-    this.getData();    
+    this.getData();
   }
 
   getData() {
     forkJoin({
-      posts$: this.dbService.getPostsWithPaging(this.rowsPerPage, this.first), 
-      count$: this.dbService.getPostsCount()
+      posts$: this.dbService.getPostsWithPaging(this.rowsPerPage, this.first),
+      count$: this.dbService.getPostsCount(),
     })
-    .subscribe((res) => {
-      // console.log('post data:', res.posts$);
-      // console.log('count data:', res.count$);
-      this.posts = res.posts$;
-      this.totalRecords = res.count$;
+      .pipe(
+        takeUntil(this.destroy$),
+        catchError((error) => {
+          console.log(error);
+          throw error;
+        })
+      )
+      .subscribe((res) => {
+        // console.log('post data:', res.posts$);
+        // console.log('count data:', res.count$);
+        this.posts = res.posts$;
+        this.totalRecords = res.count$;
 
-      // Only set recent posts for first page
-      if (this.first === 0)
-        this.recentPosts = this.posts;
-     });
+        // Only set recent posts for first page
+        if (this.first === 0) this.recentPosts = this.posts;
+      });
   }
 
   onPageChange(event: PaginatorState) {
     this.first = event.first;
     this.rowsPerPage = event.rows;
     this.getData();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
   }
 }
